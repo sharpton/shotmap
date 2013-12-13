fc73a515bcf8aef79aa8be09d5305300fa3562a9
Thomas Sharpton <sharpton@shattuck.(none)>
2013-12-13 13:12:03 -0800

    Updating statistical testing scripts....

--- scripts/R/compare_families.R
+++ scripts/R/compare_families.R
@@ -14,15 +14,16 @@ options(error=traceback)
 options(error=recover)
 
 Args              <- commandArgs()
-samp.abund.map    <- Args[4]
-metadata.tab      <- Args[6]
-family.stem       <- Args[7]
-compare.stem      <- Args[8]
+samp.abund.map    <- Args[4]  #required
+metadata.tab      <- Args[6]  #required
+family.stem       <- Args[7]  #required
+compare.stem      <- Args[8]  #required
+test.type         <- Args[9]  #optional, is auto set below if NA
 
-#For testing purposes only
+###For testing purposes only
 samp.abund.map <- "/mnt/data/work/pollardlab/sharpton/MRC_ffdb/projects/SFams_english_channel_L4/90/output/Abundance_Map_cid_54_aid_1.tab"
-metadata.tab <- "/mnt/data/work/pollardlab/sharpton/MRC_ffdb/projects/SFams_english_channel_L4/90/output/sample_metadata.tab"
-sample.stem <- "/mnt/data/work/pollardlab/sharpton/MRC_ffdb/projects/SFams_english_channel_L4/90/output/Family_Diversity_cid_54_aid_1"
+metadata.tab <- "/mnt/data/work/pollardlab/sharpton/MRC_ffdb/projects/SFams_english_channel_L4/90/output/sample_metadata_test.tab"
+family.stem <- "/mnt/data/work/pollardlab/sharpton/MRC_ffdb/projects/SFams_english_channel_L4/90/output/Family_Diversity_cid_54_aid_1"
 compare.stem <- "/mnt/data/work/pollardlab/sharpton/MRC_ffdb/projects/SFams_english_channel_L4/90/output/Compare_Families_cid_54_aid_1"
 
 ###Set autodetection thresholds
@@ -91,59 +92,105 @@ for( a in 1:length( meta.names ) ){
   types <- unique( meta[,meta.field] )
   
   if( length(types) > n.type.plot.lim ){
-    print( paste( "There are ", length(types), " types of the field <", meta.field, ">, so I will only output tables for it" ) )
+    print( paste( "There are ", length(types), " types of the field <", meta.field, ">, so I will only output tables for it", sep="" ))
     to.plot = 0
   }
-  ##right now, we only do wilcoxon on discrete classes if no more than 2 categories. Talk to KP about other options
+  ##t.test/wilcoxon.test
   if( length( types ) == 2 & autodetect( meta[,meta.field], cont.thresh ) == "discrete"){
+    #note that wilcoxon won't give significant results if number of samples for each type < 4. Just do a t-test in that case
     type.x    <- types[1]
     type.y    <- types[2]
     x.samps   <- subset( meta, meta[,meta.field] == type.x )$SAMPLE.ID
     y.samps   <- subset( meta, meta[,meta.field] == type.y )$SAMPLE.ID
+    if( is.na(test.type) ){ #if user defines it above, skip this step
+      test.type <- "wilcoxon.test"
+      if( length(x.samps) <= 3 & length(y.samps) <= 3 ){
+        print( paste( "There aren't enough samples in each type of the field <", meta.field, "> to have the power to ",
+                     "detect significant differences using a Wilcoxon Test. We'll implement a t-test instead. Note that ",
+                     "this *assumes* that the samples in each type are normally distributed!", sep="") )
+        test.type = "t.test"
+      }
+    }
+    print( paste("...preparing ", test.type, " tests...", sep=""))
     fam.map.x <- ra.map[as.character(x.samps),]
     fam.map.y <- ra.map[as.character(y.samps),]
     test.data <- t(rbind( fam.map.x, fam.map.y )) #this needs to be families by samples for multtest
-    test.type <- c( rep(0, length(x.samps) ), rep(1, length(y.samps ) ) )
-    teststat  <- mt.teststat( test.data, test.type, test="wilcoxon" )
-    ##from: http://www.bioconductor.org/packages/2.12/bioc/vignettes/multtest/inst/doc/multtest.pdf
-    rawp0           <- 2 * (1 - pnorm(abs(teststat)))    
+    classes <- c( rep(0, length(x.samps) ), rep(1, length(y.samps ) ) )
+    ##RUN THE TEST
+    ##t.test
+    if( test.type == "t.test" ){
+      rawp0 <- apply(test.data, 1, function(x) t.test(x~classes)$p.value)
+    } else if( test.type == "wilcoxon.test" ){   
+      ##fastest procedure seems to be applying wilcox.test to df rows
+      rawp0 <- apply(test.data, 1, function(x) wilcox.test(x~classes)$p.value)
+      ##from: http://www.bioconductor.org/packages/2.12/bioc/vignettes/multtest/inst/doc/multtest.pdf
+      ##teststat  <- mt.teststat( test.data, classes, test="wilcoxon" )
+      ##rawp0           <- 2 * (1 - pnorm(abs(teststat)))
+    }
     res             <- mt.rawp2adjp(rawp0, procs)
     adjp            <- res$adjp[order(res$index), ]
-    rnd.p           <- round(adjp[,1:length(procs)], 2)
-    rownames(rnd.p) <- rownames(test.data)
+    num.p           <- length(procs) + 1  #add 1 for raw.p
+    rnd.p           <- round(adjp[,1:num.p], 4)
+    rownames(rnd.p) <- rownames( test.data )
+    rnd.p2 <- data.frame( test.type=test.type, rnd.p)
     ##now do something with this output
     tab.file <- paste( family.stem, "-", meta.field, "-", type.x, "-", type.y, "-pvals.tab", sep="" )
-    write.table( rnd.p, tab.file )
+    print( paste( "Generating p-value table for ", tab.file, sep="") )
+    write.table( rnd.p2, tab.file )
     if( to.plot ){
       ##plot the results
     }
-  } else if( length( types ) > 2 & autodetect( meta[,meta.field], cont.thresh ) == "discrete" ){
-    
   }
+  ##do a Kruskal-Wallis if the number of discrete classes > 2
+  else if( length( types ) > 2 & autodetect( meta[,meta.field], cont.thresh ) == "discrete" ){
+    print("...preparing kruskal-wallis tests...")
+    classes.unsort        <-meta[,meta.field]
+    names(classes.unsort) <-meta$SAMPLE.ID
+    test.data <- t( ra.map )
+    classes   <- classes.unsort[ colnames( test.data ) ]
+    rawp0     <- apply( test.data, 1, function(x) kruskal.test( x~classes)$p.value )
+    res             <- mt.rawp2adjp(rawp0, procs)
+    adjp            <- res$adjp[order(res$index), ]
+    num.p           <- length(procs) + 1  #add 1 for raw.p
+    rnd.p           <- round(adjp[,1:num.p], 4)
+    rownames(rnd.p) <- rownames( test.data )
+    rnd.p2          <- data.frame( test.type="kruskal.test", rnd.p)
+    ##now do something with this output
+    tab.file        <- paste( family.stem, "-", meta.field, "-", type.x, "-", type.y, "-pvals.tab", sep="" )
+    print( paste( "Generating p-value table for ", tab.file, sep="") )
+    write.table( rnd.p2, tab.file )
+  }  
   ##Correlation analyses on continuous vars
-  else if( lengths(meta$SAMPLE.ID) > 2 & autodetect( meta[,meta.field], cont.thresh ) == "continuous" ){ #spearman's correlation and lm analysis, reqs 3 or more samples
+  ##just linear for now, but could fit additional models in the future
+  else if( length(meta$SAMPLE.ID) > 2 & autodetect( meta[,meta.field], cont.thresh ) == "continuous" ){ #spearman's correlation and lm analysis, reqs 3 or more samples
+    print("...preparing spearman rank correlation tests...")
     meta.vals     <- meta[order(meta$SAMPLE.ID),][,meta.field] #have to order by sample id to map to ra.map
-    res           <- as.data.frame(t(apply( t(ra.map), 1, function(row){
+    test.data     <- t(ra.map)
+    rawp0         <- as.data.frame(t(apply( test.data, 1, function(row){
       s<-cor.test( row, meta.vals, type="spearman" )
-      c(s$statistic, s$p.value)
+      c(s$estimate, s$p.value)
     } )))
-    colnames(res) <- c( "statistic", "p.value" )
-    rownames(res) <- rownames( t( ra.map ) )
+    colnames(rawp0) <- c( "cor.estimate", "p.value" )
+    rownames(rawp0) <- rownames( test.data )
     #do we need to correct for multiple tests? I think so...
-    corrected     <- mt.rawp2adjp( res$p.value, procs )
-    adjp          <- corrected$adjp[order(corrected$index), ] #according to help, this returns rows in original order in res
-    res           <- cbind( FAMID=rownames(res), res, adjp )
+    res           <- mt.rawp2adjp( rawp0$p.value, procs )
+    adjp          <- res$adjp[order(res$index), ] #according to help, this returns rows in original order in res
+    num.p         <- length(procs) + 1  #add 1 for raw.p
+    rnd.p         <- round(adjp[,1:num.p], 4)
+    rnd.cor       <- round(rawp0$cor.estimate, 4 )
+    res           <- data.frame( test.type="spearman", cor.estimate=rnd.cor, rnd.p )
     #print results to output
     tab.file      <- paste( family.stem, "-", meta.field, "-RA_spearman_statistics.tab", sep="")
-    write.table( fam.stats, file = stat.file )        
-    sigs          <- apply( res, 1, function(row){      
-      if( row[,plot.proc] <= sig.threshold ){
-        famid     <- row$FAMID
-        tab.file  <- paste( family.stem, "-", famid, "-", meta.field, "-RA_correlation_data.tab", sep="")
-        tmp.tab   <- as.data.frame( SAMPLE.ID = rownames( ra.map ), RELATIVE.ABUNDANCE = ra.map[,famid], meta.field = meta.vals )
-        write.table( tmp.tab, file = tab.file )
-      }
-    } )
+    print( paste( "Generating p-value table for ", tab.file, sep="") )
+    write.table( res, tab.file )        
+#    sigs          <- apply( res, 1, function(row){      
+#      if( row[,plot.proc] <= sig.threshold ){
+#        famid     <- row$FAMID
+#        tab.file  <- paste( family.stem, "-", famid, "-", meta.field, "-RA_correlation_data.tab", sep="")
+#        tmp.tab   <- as.data.frame( SAMPLE.ID = rownames( ra.map ), RELATIVE.ABUNDANCE = ra.map[,famid], meta.field = meta.vals )
+#        write.table( tmp.tab, file = tab.file )
+#      }
+#    } )
     ##add linear fit plot here, maybe...  
   }
 }
